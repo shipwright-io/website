da5f4dc882675f6a329cc7995dcd0187141f2f62
Adam Kaplan <adam.kaplan@redhat.com>
2026-04-06 21:29:16 -0400

    fix: Get Started Button
    Update call to action to link to the installation guide.
    
    Signed-off-by: Adam Kaplan <adam.kaplan@redhat.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,8 +20,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/introduction">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/docs/getting-started/installation">
+            Get Started 🚢 
           </Link>
         </div>
       </div>
@@ -33,8 +33,8 @@ export default function Home(): ReactNode {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Shipwright - a CNCF Sandbox Project">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
